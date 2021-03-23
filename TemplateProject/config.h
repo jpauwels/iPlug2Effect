79249cabe4adce8be1767123bf334180c5f28c25
Johan Pauwels <jpauwels@users.noreply.github.com>
2021-03-23 18:20:26 +0000

    Change template to audio effect that processes mono and stereo

--- TemplateProject/config.h
+++ TemplateProject/config.h
@@ -15,15 +15,15 @@
 
 #define SHARED_RESOURCES_SUBPATH "TemplateProject"
 
-#define PLUG_CHANNEL_IO "2-2"
+#define PLUG_CHANNEL_IO "1-1 2-2"
 
 #define PLUG_LATENCY 0
 #define PLUG_TYPE 0
-#define PLUG_DOES_MIDI_IN 1
+#define PLUG_DOES_MIDI_IN 0
 #define PLUG_DOES_MIDI_OUT 0
 #define PLUG_DOES_MPE 0
 #define PLUG_DOES_STATE_CHUNKS 0
-#define PLUG_HAS_UI 1
+#define PLUG_HAS_UI 0
 #define PLUG_WIDTH 1024
 #define PLUG_HEIGHT 768
 #define PLUG_FPS 60
@@ -40,10 +40,10 @@
 #define AAX_TYPE_IDS_AUDIOSUITE 'IEA1', 'IEA2'
 #define AAX_PLUG_MFR_STR "Acme"
 #define AAX_PLUG_NAME_STR "TemplateProject\nIPEF"
-#define AAX_PLUG_CATEGORY_STR "Synth"
+#define AAX_PLUG_CATEGORY_STR "Effect"
 #define AAX_DOES_AUDIOSUITE 1
 
-#define VST3_SUBCATEGORY "Instrument|Synth"
+#define VST3_SUBCATEGORY "Fx"
 
 #define APP_NUM_CHANNELS 2
 #define APP_N_VECTOR_WAIT 0
